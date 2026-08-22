4bf3c3c35d80538a1fa9c3862433d55e1e7b488c
Tim Schilling <schillingt@better-simple.com>
2026-08-22 14:14:55 +0000

    Configured project for deployment.

--- config/settings.py
+++ config/settings.py
@@ -220,3 +220,31 @@ if os.getenv("ON_PYTHONANYWHERE"):
     }
 
     STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+
+# PythonAnywhere settings.
+import os  # noqa: E402
+
+try:
+    from dotenv import load_dotenv
+
+    load_dotenv()
+except ImportError:
+    pass
+
+if os.getenv("ON_PYTHONANYWHERE"):
+    import dj_database_url
+
+    DEBUG = os.getenv("DEBUG") == "TRUE"
+    SECRET_KEY = os.getenv("SECRET_KEY")
+
+    try:
+        ALLOWED_HOSTS.append("*")
+    except NameError:
+        ALLOWED_HOSTS = ["*"]
+
+    DATABASES = {
+        "default": dj_database_url.config(),
+    }
+
+    STATIC_ROOT = os.path.join(BASE_DIR, "static")
